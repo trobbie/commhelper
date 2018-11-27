ba27abb95ef77ca5f9f001100182a449aacce989
Trevor Robbie <trobbie@gmail.com>
2018-11-27 18:25:10 -0500

    added apiURL for production, even if not used

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  // URL to the web api for users, activities, etc.
+  apiUrl: 'http://localhost:4200' // TODO: change when have a production server
 };
