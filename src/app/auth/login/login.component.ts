4a7885dccd46a94a046e6f5e451a388d889f3665
Trevor Robbie <trobbie@gmail.com>
2018-12-08 15:18:56 -0500

    part of #57 added unsubscriptions

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   message: string;
+  loginSub: Subscription = null;
 
   constructor(public authService: AuthService, public router: Router) {
     this.setMessage();
   }
 
   ngOnInit() {}
+
+  ngOnDestroy() {
+    if (this.loginSub) {
+      this.loginSub.unsubscribe();
+    }
+  }
 
   setMessage() {
     this.message = 'Logged ' + (this.authService.isLoggedIn ? 'in as ' + this.authService.userName : 'out');
@@ -24,7 +32,7 @@ export class LoginComponent implements OnInit {
   private loginHelper() {
     this.message = 'Trying to log in ...';
 
-    this.authService.login().subscribe(() => {
+    this.loginSub = this.authService.login().subscribe(() => {
       this.setMessage();
       if (this.authService.isLoggedIn) {
         // Get the redirect URL from our auth service
